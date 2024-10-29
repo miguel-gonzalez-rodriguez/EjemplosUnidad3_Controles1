4f756b9c430dad1df44d0f5225ba7ad6c0691622
miguel <migonrod@gmail.com>
2024-10-29 09:49:37 +0100

    Añadidos comentarios explicando el código de MainActivity.java

--- app/src/main/java/com/example/ejemplosunidad3/MainActivity.java
+++ app/src/main/java/com/example/ejemplosunidad3/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.ejemplosunidad3;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.UnderlineSpan;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -32,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        /*
+        * Sección sobre Spans
+        * Se instancia un TextView creado con XML y se modifica su contenido con SpanStringBuilder
+        * */
+
         // Objeto con la vista
         TextView textView = (TextView) findViewById(R.id.hola_mundo);
         
@@ -39,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         // Para añadirle etiquetas de estilo a la cadena, podemos usar las clases Span
         SpannableStringBuilder cadena = new SpannableStringBuilder("nuevo texto");
         cadena.append(" más texto");
-        
+
         // Se añaden las etiquetas a la cadena
         cadena.setSpan(new UnderlineSpan(),0,cadena.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
 
@@ -51,10 +59,19 @@ public class MainActivity extends AppCompatActivity {
         // Establece el texto a la vista
         textView.setText(cadena);
 
-
+        /*
+         * Sección sobre Fuentes de texto
+         * Se ha añadido anterioremente la fuente en el Resource Manager.
+         * Y la asignamos programáticamente a un TextView
+         * */
         // Establecer un nuevo tipo de Fuente
 //        Typeface nuevaFuente = getResources().getFont(R.font.tsukimi_rounded);
 //        textView.setTypeface(nuevaFuente);
+
+        /*
+         * Sección sobre Animaciones
+         * Ejemplo de animación de aparición de texto
+         * */
 
         // Ejemplo animación de TextView
 //        AnimationSet animationSet = new AnimationSet(true);
@@ -64,23 +81,33 @@ public class MainActivity extends AppCompatActivity {
 //        textView.startAnimation(animationSet);
 
 
-        Button bt_eventos = (Button) findViewById(R.id.button2);
-        bt_eventos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Eventos.class);
-                startActivity(intent);
-            }
-        });
+        /*
+         * Sección sobre Eventos
+         * Se añade un evento a un botón mediante una clase anónima
+         * */
 
-        Button bt_radios = (Button) findViewById(R.id.buttonRadio);
-        bt_radios.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RadioButtons.class);
-                startActivity(intent);
-            }
-        });
+        // Instancia el botón
+//        Button bt_eventos = (Button) findViewById(R.id.button2);
+
+        // Añade el listener (receptor) y define el handler (manejador) con su método de callback
+//        bt_eventos.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this,Eventos.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        // Mismo ejemplo con otro botón
+        // Con una clase anónima tenemos que añadir los listner de manera individual a cada vista
+//        Button bt_radios = (Button) findViewById(R.id.buttonRadio);
+//        bt_radios.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, RadioButtons.class);
+//                startActivity(intent);
+//            }
+//        });
 
     }
 }
