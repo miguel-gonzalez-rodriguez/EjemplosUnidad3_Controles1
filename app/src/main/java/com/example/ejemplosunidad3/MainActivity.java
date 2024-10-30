88ae343035ceaf136097afda5cf8b3bac12923c2
Miguel <migonrod@edu.xunta.gal>
2024-10-30 15:57:33 +0100

    Después de clase

--- app/src/main/java/com/example/ejemplosunidad3/MainActivity.java
+++ app/src/main/java/com/example/ejemplosunidad3/MainActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.AnimationSet;
 import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -21,7 +20,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity //implements View.OnClickListener
+{
+    private class ReceptorBoton implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            System.out.println("implementado con clase interna");
+        }
+    }
+
+//    @Override
+//    public void onClick(View view) {
+//        System.out.println("dsfadsfdsfadsfdsaffads");
+//        if (view.getId() == findViewById(R.id.button2)){
+//
+//        }
+//    }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -35,19 +49,41 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        /*
-        * Sección sobre Spans
-        * Se instancia un TextView creado con XML y se modifica su contenido con SpanStringBuilder
-        * */
+        Button button = findViewById(R.id.button2);
+
+
+        // la misma clase
+        //button.setOnClickListener(this);
+        ReceptorBoton listener = new ReceptorBoton();
+
+        button.setOnClickListener(listener);
+
+        Button bt = findViewById(R.id.buttonRadio);
+        bt.setOnClickListener(listener);
+
+
+        button.setOnClickListener(view -> {
+            System.out.println("clase anónima");
+        });
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("clase anónima otra diferente");
+            }
+        });
+
+
+
 
         // Objeto con la vista
-        TextView textView = (TextView) findViewById(R.id.hola_mundo);
+        TextView textView = findViewById(R.id.hola_mundo);
         
         // El objeto StringBuilder facilita el uso de cadenas
         // Para añadirle etiquetas de estilo a la cadena, podemos usar las clases Span
         SpannableStringBuilder cadena = new SpannableStringBuilder("nuevo texto");
         cadena.append(" más texto");
-
+        
         // Se añaden las etiquetas a la cadena
         cadena.setSpan(new UnderlineSpan(),0,cadena.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
 
@@ -59,37 +95,22 @@ public class MainActivity extends AppCompatActivity {
         // Establece el texto a la vista
         textView.setText(cadena);
 
-        /*
-         * Sección sobre Fuentes de texto
-         * Se ha añadido anterioremente la fuente en el Resource Manager.
-         * Y la asignamos programáticamente a un TextView
-         * */
-        // Establecer un nuevo tipo de Fuente
-//        Typeface nuevaFuente = getResources().getFont(R.font.tsukimi_rounded);
-//        textView.setTypeface(nuevaFuente);
 
-        /*
-         * Sección sobre Animaciones
-         * Ejemplo de animación de aparición de texto
-         * */
+        // Establecer un nuevo tipo de Fuente
+        Typeface nuevaFuente = getResources().getFont(R.font.tsukimi_rounded);
+//        Typeface otrra = getResources().getFont(R.font.otra);
+        textView.setTypeface(nuevaFuente);
 
         // Ejemplo animación de TextView
-//        AnimationSet animationSet = new AnimationSet(true);
-//        AlphaAnimation aparicion = new AlphaAnimation(0,1);
-//        aparicion.setDuration(3000);
-//        animationSet.addAnimation(aparicion);
-//        textView.startAnimation(animationSet);
+        AnimationSet animationSet = new AnimationSet(true);
+        AlphaAnimation aparicion = new AlphaAnimation(0,1);
+        aparicion.setDuration(3000);
+        animationSet.addAnimation(aparicion);
 
+        textView.startAnimation(animationSet);
 
-        /*
-         * Sección sobre Eventos
-         * Se añade un evento a un botón mediante una clase anónima
-         * */
-
-        // Instancia el botón
-//        Button bt_eventos = (Button) findViewById(R.id.button2);
-
-        // Añade el listener (receptor) y define el handler (manejador) con su método de callback
+//
+//        Button bt_eventos = findViewById(R.id.button2);
 //        bt_eventos.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -98,16 +119,7 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-        // Mismo ejemplo con otro botón
-        // Con una clase anónima tenemos que añadir los listner de manera individual a cada vista
-//        Button bt_radios = (Button) findViewById(R.id.buttonRadio);
-//        bt_radios.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, RadioButtons.class);
-//                startActivity(intent);
-//            }
-//        });
-
     }
+
+
 }
